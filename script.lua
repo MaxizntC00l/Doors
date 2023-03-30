7d007ecee22161e0610bf1513ca4d675a44de8e1
Max <97249285+MaxizntC00l@users.noreply.github.com>
2023-03-29 18:12:17 -0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,36 @@
-local entiy = ""
+local function Trigger(Title,Desc,Reason)
+    local Details = {
+        Description = Desc,
+        Title = Title,
+        Reason =  Reason
+    }
+    local LocalPlayer = game.Players.LocalPlayer;
+    local GUI = LocalPlayer.PlayerGui.MainUI
+    local TweenService = game:GetService("TweenService");
+    local AchievementsHolder = GUI.AchievementsHolder.Achievement:Clone();
 
-task.spawn(function()
-    workspace.ChildAdded:Connect(function(monster)
-        
-    end)
-end)
+
+    AchievementsHolder.Size = UDim2.new(0, 0, 0, 0);
+    AchievementsHolder.Frame.Position = UDim2.new(1.1, 0, 0, 0);
+    AchievementsHolder.Name = "LiveAchievement";
+    AchievementsHolder.Visible = true;
+    AchievementsHolder.Parent = GUI.AchievementsHolder;
+    AchievementsHolder.Frame.Details.Desc.Text = Details.Description
+    AchievementsHolder.Frame.Details.Title.Text = Details.Title
+    AchievementsHolder.Frame.Details.Reason.Text = Details.Reason
+    AchievementsHolder.Frame.ImageLabel.Image:Destroy()
+
+    AchievementsHolder.Sound:Play();
+    AchievementsHolder:TweenSize(UDim2.new(1, 0, 0.2, 0), "In", "Quad", 0.8, true);
+    wait(0.8);
+    AchievementsHolder.Frame:TweenPosition(UDim2.new(0, 0, 0, 0), "Out", "Quad", 0.5, true);
+    TweenService:Create(AchievementsHolder.Frame.Glow, TweenInfo.new(1, Enum.EasingStyle.Quad, Enum.EasingDirection.In), {
+        ImageTransparency = 1
+            }):Play();
+            wait(8);
+    AchievementsHolder.Frame:TweenPosition(UDim2.new(1.1, 0, 0, 0), "In", "Quad", 0.5, true);
+    wait(0.5);
+    AchievementsHolder:TweenSize(UDim2.new(1, 0, -0.1, 0), "InOut", "Quad", 0.5, true);
+    wait(0.5);
+    AchievementsHolder:Destroy();
+end
